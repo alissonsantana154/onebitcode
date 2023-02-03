1d7d9ecf863babe7a2126086db76afc8a6262cc6
Alisson Santana <alissonsantana16608@gmail.com>
2023-02-03 15:01:22 -0300

    Recursos nativos do navegador

---
+++ js/modulo-1/recursos-nativos-do-navegador/index.js
@@ -0,0 +1,9 @@
+let nome = "Alisson";
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+nome = window.prompt("Insira seu nome:");
+window.alert(nome);
+let cofirmacao = window.confirm("Cofima essa ação?");
