8dad899e0e160b67669dd90f14046c85c9bee2e9
Alisson Santana <alissonsantana16608@gmail.com>
2023-03-22 12:40:03 -0300

    Página 404

--- react/lorem-app/src/routes.js
+++ react/lorem-app/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PlanetsScreen from "./screens/planets";
 import PlanetScreen from "./screens/planet";
+import NotFoundScreen from "./screens/notFound";
 
 const Rotas = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" Component={PlanetsScreen} />
       <Route exact path="/planet/:id" Component={PlanetScreen} />
+      <Route path="*" Component={NotFoundScreen} />
     </Routes>
   </BrowserRouter>
 );
---
+++ react/lorem-app/src/screens/notFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Planet from "../Components/planet";
+
+import { Link } from "react-router-dom";
+
+const NotFoundScreen = () => {
+  return (
+    <div>
+      <h3>Página não encontrada!</h3>
+      <Link to="/">Voltar para listagem</Link>
+    </div>
+  );
+};
+
+export default NotFoundScreen;
