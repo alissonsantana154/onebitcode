5f7163557199851c8e701abad4923b2304f95a36
Alisson Santana <alissonsantana16608@gmail.com>
2023-03-22 15:17:02 -0300

    Manipulando arquivos

---
+++ node/manipulando-arquivos/file.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// cria um arquivo e sobre escreve o arquivo enteiro
+// fs.writeFile("test.txt", "Olá NodeJs", (err) => {
+//   console.log(err);
+// });
+
+// cria um arquivo e coloca conteudos adicionais
+// fs.appendFile("test.txt", "Olá NodeJs\n", (err) => {
+//   console.log(err);
+// });
+
+// renomea um arquivo
+// fs.rename("test.txt", "test2.txt", (err) => console.log(err));
+
+// remove um arquivo
+// fs.unlink("test2.txt", (err) => console.log(err));
+
+// mostra o caminho do diretorio atual
+console.log(__dirname);
