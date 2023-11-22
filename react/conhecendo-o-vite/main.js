e9f477d75e67ffb73dc2ac96e2703727e98b97bc
Alisson Santana <alissonsantana16608@gmail.com>
2023-11-22 16:39:17 -0300

    Criando um projeto React com Vite

--- react/conhecendo-o-vite/main.js
+++ react/conhecendo-o-vite/main.js
@@ -6,6 +6,7 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <h2>Ola,mundo mundo</>
   <div>
+
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
--- react/first-react-project/src/App.jsx
+++ react/first-react-project/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Ola, mundo!</h1>
+      <p>Este é meu primeiro projeto usando React e Vite!</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
   )
 }
-
-export default App
