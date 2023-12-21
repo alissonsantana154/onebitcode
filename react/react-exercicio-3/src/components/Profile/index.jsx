77a13a986f08f9745f068f785b885519e814f52e
Alisson Santana <alissonsantana16608@gmail.com>
2023-12-21 10:21:45 -0300

    Usando Eventos do JavaScript no React

--- react/react-exercicio-3/src/components/Profile/index.jsx
+++ react/react-exercicio-3/src/components/Profile/index.jsx
@@ -3,13 +3,23 @@ import Title from "../Title"
 import ProfileSection from "./ProfileSection"
 import styles from "./styles.module.css"
 
+function handleClick(ev) {
+  console.log(ev)
+  alert("Você agora está seguindo!")
+}
+
 export default function Profile(props) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
       <Title>
         <span>{props.name}</span>
-        <button>Follow</button>
+        <button
+          className={styles.followButton}
+          onClick={handleClick}
+        >
+          Follow
+        </button>
       </Title>
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
