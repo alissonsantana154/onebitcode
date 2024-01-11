527f379e7080741d037a762aa865ab80567caaee
Alisson Santana <alissonsantana16608@gmail.com>
2024-01-11 09:53:30 -0300

    Lidando com Data Fetching e Erros

--- react/trabalhando-com-spas/src/pages/Product.jsx
+++ react/trabalhando-com-spas/src/pages/Product.jsx
@@ -1,16 +1,8 @@
-import { Link, useParams } from "react-router-dom"
-import products from "../database.json"
+import { Link, useLoaderData,  } from "react-router-dom"
 
 export default function Product() {
-  const { productId } = useParams()
+const product = useLoaderData()
 
-  const product = products.find(p => p.id === +productId)
-
-  if (!product) {
-    return (
-      <h2>Ooops... Esse produto não foi encontrado =(</h2>
-    )
-  }
   return (
     <section>
       <Link to="/products">
